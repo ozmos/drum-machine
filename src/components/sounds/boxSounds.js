c726047f35a3d751d0eee84c82e209a3e3339636
ozmos <osamumorozumi@gmail.com>
2018-11-03 16:41:33 +0800

    fixed beat box kit

--- src/components/sounds/boxSounds.js
+++ src/components/sounds/boxSounds.js
@@ -2,6 +2,6 @@ function importAll(r) {
     return r.keys().map(r);
 }
 
-const tablaSounds = importAll(require.context('./tablaKit/', false, /\.(mp3)$/));
+const tablaSounds = importAll(require.context('./beatBoxClips/', false, /\.(mp3)$/));
 
 export default tablaSounds;
