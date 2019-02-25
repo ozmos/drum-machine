a1c05e95d30ce2eacae6b7050f231bfd2bee30e5
ozmos <osamumorozumi@gmail.com>
2019-02-25 10:48:28 +0800

    old changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import Pads from './components/Pads';
 import { connect } from 'react-redux';
 import Display from './components/Display';
